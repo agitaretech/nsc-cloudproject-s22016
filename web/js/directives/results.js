f936fa122951127dbb26c3081b604a88ba03de42
KSENIA IVANTSOVA <kivantsova@outlook.com>
2016-02-17 00:59:16 -0800

    Angular directive for list of images implemented
    Now we need to put the CRUD angular factory into app.js,
    this commit prepared the infrastructure

--- web/js/directives/results.js
+++ web/js/directives/results.js
@@ -1,7 +1,29 @@
 
-app.directive("results", function() {
-    return {
-        restrict : "A",
-        template : "<h1>Made by a directive!</h1>"
+
+app.controller('resultsController', 
+
+ function($scope, $http) {
+	 
+  $http({method: 'GET', url: 'postsproto.json'}).success(function(data) {
+$scope.posts = data;
+ $scope.pickedImage = {
+        url: ""
     };
+
+   $scope.getPicked = function () {
+        return $scope.picked;
+    };
+
 });
+
+
+
+  }).directive('results', function() {
+  return {
+	  restrict: 'E',
+        replace: true,
+    templateUrl: 'js/directives/results.html'
+  };
+});
+
+
