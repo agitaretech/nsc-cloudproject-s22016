f6bcf619c7c85bc68b15d4c7b936248d43ddd68a
KSENIA IVANTSOVA <kivantsova@outlook.com>
2016-02-17 23:28:41 -0800

    routing started in app.js
    started doing he routing

--- web/js/app.js
+++ web/js/app.js
@@ -1,7 +1,25 @@
 
 //var ngResource = require('ng-resource');
-var app = angular.module('myApp', []);
-//var services = angular.module('ngdemo.services', ['ngResource']);
-
-
+var app = angular.module('app', ['ngRoute']);
+var module=angular.module('module', []);
+app.config(function($routeProvider){
+	$routeProvider
+	// route for the home page
+            .when('/', {
+                templateUrl : 'index.html',
+                controller  : 'resultsController'
+            })
+	   .when('/upload', {
+                templateUrl : 'angular_spa.html',
+                controller  : 'uploadController'
+            })
+			   .when('/gallery', {
+                templateUrl : 'index.html',
+                controller  : 'resultsController'
+            });
+	
+	
+	
+	
+	});
 
