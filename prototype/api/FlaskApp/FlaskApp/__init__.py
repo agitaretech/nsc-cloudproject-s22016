b05d3ca77da18d92fd6df1feb907265fe60f8e6a
RJ Hunter <rjhunter@RJ-Hunters-MacBook-Pro.local>
2016-03-01 00:31:18 -0800

    rh api update

--- prototype/api/FlaskApp/FlaskApp/__init__.py
+++ prototype/api/FlaskApp/FlaskApp/__init__.py
@@ -3,53 +3,72 @@ from flask import request
 from flask import jsonify
 from azure_components.api_methods import *
 
+#declare flask app
 app = Flask(__name__)
 
-
+#route for get images call
 @app.route("/getImages", methods=['GET'])
 def getImages():
+    #header variables for get api method
     timestamp = request.headers.get('timestamp')
     tags = request.headers.get('tags')
     username = request.headers.get('username')
     token = request.headers.get('token')
     secret = request.headers.get('secret')
     prev = request.headers.get('prev')
+    # checks if prev variable is set
     if opt_param is None:
-        prev = false
+        # updates variable to false
+        prev = 'false'
+    #calls get image method to get JSON from api method (get)
     rtn_json = getImagesJSON(timestamp, prev, tags, username, token, secret)
+    # return json request
     return jsonify(request=rtn_json)
 
+#route for uploading images
 @app.route("/uploadImage", methods=['POST'])
 def uploadImage():
+    #header variables for post api method
     username = request.headers.get('username')
     blob = request.headers.get('blob')
     filename = request.headers.get('filename')
     token = request.headers.get('token')
     secret = request.headers.get('secret')
     tags = request.headers.get('tags')
-    
+
+    #calls upload image method (post)
     rtn_json = uploadImageJSON(username, blob, filename, token, secret, tags)
+    #returns json succes or error json message
     return jsonify(request=rtn_json)
 
+#route for deleting images
 @app.route("/deleteImage", methods=['DELETE'])
 def deleteImage():
+    #header variables for delete api method
     blobURL = request.headers.get('blobURL')
     token = request.headers.get('token')
     secret = request.headers.get('secret')
+
+    #calls delete image method (delete)
     rtn_json = deleteImageJSON(username, blobURL, token, secret)
+    #returns json succes or error json message
     return jsonify(request=rtn_json)
 
+#route for updating tags
 @app.route("/updateTags", methods=['PUT'])
 def updateTags():
+    #header variables for put api method
     blobURL = request.headers.get('blobURL')
     tags = request.headers.get('tags')
     token = request.headers.get('token')
     secret = request.headers.get('secret')
 
-    
+    #calls update tags method (put)
     rtn_json = updateTagsJSON(blobURL, tags, username, token, secret)
+    #returns json succes or error json message
     return jsonify(request=rtn_json)
 
+#initializes app and runs debug
 if __name__ == "__main__":
     app.debug=True 
     app.run()
---
+++ prototype/api/FlaskApp/FlaskApp/azure_components/static/app_keys.py
@@ -0,0 +1,13 @@
+#blob account credentials
+blob_account_name = ''
+blob_account_key = ''
+
+#twitter app oauth credentials
+oauth_consumer_key = ''
+oauth_consumer_secret = ''
+
+#documentdb credentials
+db_client = ''
+db_client_key = ''
+db_name = ''
+db_collection = ''
