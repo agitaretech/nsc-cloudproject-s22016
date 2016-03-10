2b2f0e6a6acddd8408cbf44380c068c93663d3ea
jesse furlan <jdfurlan@gmail.com>
2016-03-09 17:39:02 -0800

    removed key

--- prototype/api/FlaskApp/FlaskApp/azure_components/static/app_keys.py
+++ prototype/api/FlaskApp/FlaskApp/azure_components/static/app_keys.py
@@ -8,6 +8,6 @@ oauth_consumer_secret = ''
 
 #documentdb credentials
 db_client = 'https://jdf-documentdb.documents.azure.com/'
-db_client_key = 'p7En1EXv+xulmP2UV8A4K+Jb8PwHDzZV9jlpncDu+xB1O7gFfpSXDcl8oJsTgDJziZ1wzUkJbDqN/he67YWuDA=='
+db_client_key = ''
 db_name = 'documents'
 db_collection = 'images'
