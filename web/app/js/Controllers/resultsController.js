beccd0278e6d781c26b0fefe5a98e3982384d399
KSENIA IVANTSOVA <kivantsova@outlook.com>
2016-03-14 05:02:03 -0700

    http get request implemented
    CORS blocking the request

--- web/app/js/Controllers/resultsController.js
+++ web/app/js/Controllers/resultsController.js
@@ -1,13 +1,44 @@
 
- 
+// app.controller('timelineController', function ($scope,
+//                                         $rootScope) {
+//  $rootScope.twitter.get('/1.1/statuses/home_timeline.json')
+//  .done(function(data) {
+//     $scope.tw_timeline = data;
+//     $scope.$apply();
+//  });
+//});
+
 app.controller('resultsController', 
 
- function($scope, $http) {
 
+
+
+
+ function($scope,  $http,$rootScope) {
+	
 	 
+		 
+	
+			$http({
+                method: 'GET',
+                headers:{
+				
+					'username':'mobiletestgrou1',
+                        'token': $rootScope.twitter.oauth_token,
+                        'secret':$rootScope.twitter.oauth_token_secret
+                        
+                         },
+                url: 'http://ad440api.cloudapp.net/getImages'
+                }).then(function successCallback(response) {
+                                  console.log(response);
+                    }, function errorCallback(response) {
+ 
+                       });	 
+	 
+
 $http({method: 'GET', url: 'postsproto.json'}).success(function(data) {
 $scope.posts = data;
- $scope.pickedImage = {
+$scope.pickedImage = {
         url: ""
     };
 
--- web/app/js/app.js
+++ web/app/js/app.js
@@ -6,8 +6,20 @@
 
 
 var app = angular.module('app', ['ngRoute']);
+app.constant('config', {appName:''});
+app.constant('config', {appToken:''});
+app.constant('config', {appSecret:''});
+
+
+app.config(['$httpProvider', function ($httpProvider) {
+	$httpProvider.defaults.useXDomain = true;
+     delete $httpProvider.defaults.headers.common['X-Requested-With'];
+     delete $httpProvider.defaults.headers.post['Content-type'];
+	delete $httpProvider.defaults.headers.post['Cache-Control'];
+}]);
 
 app.config(function($routeProvider) {
+	
 	$routeProvider.when('/timeline', {
 	    templateUrl: '/js/Directives/timeline.html',
 	    controller: 'timelineController'
@@ -16,8 +28,11 @@ app.config(function($routeProvider) {
 	    controller: 'connectController'
 	}).when('/results', {
 	    templateUrl: '/js/Directives/results/results.html',
-	    controller: 'resultsController'}
-	).otherwise({
+	    controller: 'resultsController'
+	}).when('/upload', {
+	    templateUrl: '/js/Directives/upload.html',
+	    controller: 'uploadController'	
+	}).otherwise({
             redirectTo: '/connect'
 	});
 });
